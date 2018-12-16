dea5f5629e613453e1bdf04d0f6e3bb927853acd
Sasheem <sts12@me.com>
2018-12-16 13:48:30 -0500

    notes, connect resolvers to apollo server

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,14 +1,18 @@
 const { ApolloServer } = require('apollo-server');
 
 const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 const { createStore } = require('./utils');
 const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
 
+// creates a database
 const store = createStore();
 
 const server = new ApolloServer({ 
     typeDefs,
+    resolvers,
+    // connect to graph
     dataSources: () => ({
         LaunchAPI: new LaunchAPI(),
         UserAPI: new UserAPI({ store }),
