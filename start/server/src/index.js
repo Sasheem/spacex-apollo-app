c2c2b250526ffec68152af7bf2ca4650028d00eb
Sasheem <sts12@me.com>
2018-12-17 12:35:33 -0500

    authenticate user, don't use in real world production

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer } = require('apollo-server');
+const isEmail = require('isemail');
 
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
@@ -9,7 +10,23 @@ const UserAPI = require('./datasources/user');
 // creates a database
 const store = createStore();
 
-const server = new ApolloServer({ 
+const server = new ApolloServer({
+    context: async ({ req }) => {
+        // simple auth check on every request
+        const auth = (req.headers && req.headers.authorization) || '';
+        const email = new Buffer(auth, 'base64').toString('ascii');
+
+        // if email isn't formatted correctly, return null
+        if (!isEmail.validate(email)) return { user: null };
+
+        // find a user by email
+        const users = await store.users.findOrCreate({ where: { email } });
+        const user = users && users[0] ? users[0] : null;
+
+        return { user: { ...user.dataValues } };
+        // DO NOT USE the specific code above in production, not secure
+        // the concepts are transferable to auth in real world applications tho
+    },  
     typeDefs,
     resolvers,
     // connect to graph
