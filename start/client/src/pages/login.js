316926c6c27caacc52862aef560556ed28028698
Sasheem <sts12@me.com>
2019-01-02 01:05:35 -0500

    expose apollo client with apollo consumer

---
+++ start/client/src/pages/login.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Mutation, ApolloConsumer } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import LoginForm from '../components/login-form';
+
+const LOGIN_USER = gql`
+    mutation login($email: String!) {
+        login(email: $email)
+    }
+`;
+
+export default function Login() {
+    return (
+        <ApolloConsumer>
+            {client => (
+                <Mutation 
+                    mutation={LOGIN_USER}
+                    onCompleted={({ login }) => {
+                        localStorage.setItem('token', login);
+                        client.writeData({ data: { isLoggedIn: true } });
+                    }}
+                >
+                    {(login, { loading, error }) => {
+                        // loading will probably never render
+                        if (loading) return <Loading />;
+                        if (error) return <p>ERROR: {error.message}</p>
+
+                        return <LoginForm login={login} />;
+                    }}
+                </Mutation>
+            )}
+        </ApolloConsumer>
+    );
+}
