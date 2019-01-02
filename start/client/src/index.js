5a2c56469e701b73c21ddd306f4da3f27999bda9
Sasheem <sts12@me.com>
2019-01-02 01:50:25 -0500

    write local schema

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient, { HttpLink } from "apollo-boost";
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import gql from "graphql-tag";
 import { ApolloProvider } from 'react-apollo';
 
+import Pages from './pages';
+import Login from './pages/login';
+
+
+const cache = new InMemoryCache();
 const client = new ApolloClient({
   cache,
   link: new HttpLink({
@@ -32,4 +38,5 @@ client
 ReactDOM.render(
   <ApolloProvider client={client}>
     <p>Hello world from apollo</p>
+    <Login />
   </ApolloProvider>, document.getElementById('root'));
