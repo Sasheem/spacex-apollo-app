c09dc21d432bb8729f8d50b67e0b6dd486ebe1b8
Sasheem <sts12@me.com>
2019-01-02 13:29:28 -0500

    created logout button that direct cache writes

---
+++ start/client/src/containers/logout-button.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'react-emotion';
+import { ApolloConsumer } from 'react-apollo';
+
+import { menuItemClassName } from '../components/menu-item';
+import { ReactComponent as ExitIcon } from '../assets/icons/exit.svg';
+
+export default function LogoutButton() {
+    return (
+        <ApolloConsumer>
+            {client => (
+                <StyledButton
+                    onClick={() => {
+                        client.writeData({ data: { isLoggedIn: false } });
+                        localStorage.clear();
+                    }}
+                >
+                    <ExitIcon />
+                    Logout
+                </StyledButton>
+            )}
+        </ApolloConsumer>
+    );
+}
+
+// styled components used in this file are below
+
+const StyledButton = styled('button')(menuItemClassName, {
+    background: 'none',
+    border: 'none',
+    padding: 0,
+});
